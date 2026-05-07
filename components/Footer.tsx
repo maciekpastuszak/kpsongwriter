c108e11514682c62090af64cc534caa9547eee32
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2026-05-07 09:44:56 +0200

    typo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="border-t border-primary/10 bg-black py-6 text-center text-sm text-muted-foreground">
-      © {new Date().getFullYear()} Krzysztof Maciej Pastuszak? Wszystkie prawa
+      © {new Date().getFullYear()} Krzysztof Maciej Pastuszak. Wszystkie prawa
       zastrzeżone.
     </footer>
   );

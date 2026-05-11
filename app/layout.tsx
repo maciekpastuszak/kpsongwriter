b9ed1ed843c838ab76fdafd841fc287f86fd81b0
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2026-05-11 12:36:47 +0200

    update Google Analytics tracking ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
-      <GoogleAnalytics gaId="G-GL5FEM8BKR" />
+      <GoogleAnalytics gaId="G-MQQJ702HHN" />
     </html>
   );
 }
